48cf64c147941b5a0f1b3774aefddc3421ff099b
Avraham Shalev <8184528+avrahams@users.noreply.github.com>
2023-08-27 11:34:13 +0300

    rem topics
    Signed-off-by: Avraham Shalev <8184528+avrahams@users.noreply.github.com>

--- pulsar/common/topics/topicnames.go
+++
@@ -1,9 +0,0 @@
-package topics
-
-import "github.com/kubescape/messaging/pulsar/connector"
-
-// Known topics names
-const (
-	//Event broadcaster topic
-	TopicNameBroadcastEvent connector.TopicName = "broadcast-event-V1"
-)
---
+++ pulsar/connector/topics.go
@@ -0,0 +1,27 @@
+package connector
+
+import (
+	"fmt"
+)
+
+type TopicName string
+
+type TopicPersistency string
+
+const (
+	//topic persistency prefix
+	TopicTypePersistent    TopicPersistency = "persistent"
+	TopicTypeNonPersistent TopicPersistency = "non-persistent"
+)
+
+func BuildTopic(persistency TopicPersistency, tenant, namespace string, topicName TopicName) string {
+	return fmt.Sprintf("%s://%s/%s/%s", persistency, tenant, namespace, topicName)
+}
+
+func BuildPersistentTopic(tenant, namespace string, topicName TopicName) string {
+	return BuildTopic(TopicTypePersistent, tenant, namespace, topicName)
+}
+
+func BuildNonPersistentTopic(tenant, namespace string, topicName TopicName) string {
+	return BuildTopic(TopicTypePersistent, tenant, namespace, topicName)
+}
