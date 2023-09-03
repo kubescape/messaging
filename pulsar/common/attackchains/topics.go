115c55d1ac9cc844ba91280ebc108f59df89145a
YiscahLevySilas1 <yiscahls@armosec.io>
2023-09-03 18:02:51 +0300

    more consts
    Signed-off-by: YiscahLevySilas1 <yiscahls@armosec.io>

---
+++ pulsar/common/attackchains/topics.go
@@ -0,0 +1,21 @@
+package attackchains
+
+const (
+	// topic names
+	AttackChainStateScanStateTopic   = "attack-chain-scan-state-v1"
+	AttackChainStateViewedTopic      = "attack-chain-viewed-v1"
+	KubescapeScanReportFinishedTopic = "kubescape-scan-report-finished-v1"
+)
+
+// --------- Ingesters structs and consts -------------
+
+// supported topics and properties:
+// [topic]/[propName]/[propValue]
+
+// attack-chain-scan-state-v1/action/update
+// attack-chain-viewed-v1/action/update
+
+const (
+	MsgPropAction            = "action"
+	MsgPropActionValueUpdate = "update"
+)
--- pulsar/common/topics.go
+++
@@ -1,8 +0,0 @@
-package common
-
-const (
-	// topic names
-	AttackChainStateScanStateTopic   = "attack-chain-scan-state-v1"
-	AttackChainStateViewedTopic      = "attack-chain-viewed-v1"
-	KubescapeScanReportFinishedTopic = "kubescape-scan-report-finished-v1"
-)
