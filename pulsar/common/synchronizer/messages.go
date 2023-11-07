024972827076c4c515c9ce1fa2e17af1607965d0
Amir Malka <amirm@armosec.io>
2023-11-07 13:24:00 +0200

    added message properties
    Signed-off-by: Amir Malka <amirm@armosec.io>

--- pulsar/common/synchronizer/messages.go
+++ pulsar/common/synchronizer/messages.go
@@ -1,6 +1,11 @@
 package synchronizer
 
 const (
+	MsgPropTimestamp = "timestamp"
+	// MsgPropCluster is the property name for the cluster name
+	MsgPropCluster = "cluster"
+	// MsgPropAccount is the property name for the account name
+	MsgPropAccount = "account"
 	// MsgPropEvent is the property name for the event type
 	MsgPropEvent                            = "event"
 	MsgPropEventValueGetObjectMessage       = "GetObject"
@@ -16,78 +21,78 @@ const (
 // structs for Pulsar messages for synchronizer topic
 
 type DeleteObjectMessage struct {
-	ClusterName  string `json:"clusterName"`
-	CustomerGUID string `json:"customerGUID,omitempty" bson:"customerGUID,omitempty"`
-	Depth        int    `json:"depth"`
-	Kind         string `json:"kind"`
-	MsgId        string `json:"msgId"`
-	Name         string `json:"name"`
+	Cluster string `json:"cluster"`
+	Account string `json:"account"`
+	Depth   int    `json:"depth"`
+	Kind    string `json:"kind"`
+	MsgId   string `json:"msgId"`
+	Name    string `json:"name"`
 }
 
 type GetObjectMessage struct {
-	BaseObject   []byte `json:"baseObject"`
-	ClusterName  string `json:"clusterName"`
-	CustomerGUID string `json:"customerGUID,omitempty" bson:"customerGUID,omitempty"`
-	Depth        int    `json:"depth"`
-	Kind         string `json:"kind"`
-	MsgId        string `json:"msgId"`
-	Name         string `json:"name"`
+	BaseObject []byte `json:"baseObject"`
+	Cluster    string `json:"cluster"`
+	Account    string `json:"account"`
+	Depth      int    `json:"depth"`
+	Kind       string `json:"kind"`
+	MsgId      string `json:"msgId"`
+	Name       string `json:"name"`
 }
 
-type NewChecksumMessage struct { // How is this different from VerifyObjectMessage?
-	Checksum     string `json:"checksum"`
-	ClusterName  string `json:"clusterName"`
-	CustomerGUID string `json:"customerGUID,omitempty" bson:"customerGUID,omitempty"`
-	Depth        int    `json:"depth"`
-	Kind         string `json:"kind"`
-	MsgId        string `json:"msgId"`
-	Name         string `json:"name"`
+type NewChecksumMessage struct {
+	Checksum string `json:"checksum"`
+	Cluster  string `json:"cluster"`
+	Account  string `json:"account"`
+	Depth    int    `json:"depth"`
+	Kind     string `json:"kind"`
+	MsgId    string `json:"msgId"`
+	Name     string `json:"name"`
 }
 
 type NewObjectMessage struct {
-	ClusterName  string `json:"clusterName"`
-	CustomerGUID string `json:"customerGUID,omitempty" bson:"customerGUID,omitempty"`
-	Depth        int    `json:"depth"`
-	Kind         string `json:"kind"`
-	MsgId        string `json:"msgId"`
-	Name         string `json:"name"`
-	Object       []byte `json:"patch"`
+	Cluster string `json:"cluster"`
+	Account string `json:"account"`
+	Depth   int    `json:"depth"`
+	Kind    string `json:"kind"`
+	MsgId   string `json:"msgId"`
+	Name    string `json:"name"`
+	Object  []byte `json:"patch"`
 }
 
 type PatchObjectMessage struct {
-	Checksum     string `json:"checksum"`
-	ClusterName  string `json:"clusterName"`
-	CustomerGUID string `json:"customerGUID,omitempty" bson:"customerGUID,omitempty"`
-	Depth        int    `json:"depth"`
-	Kind         string `json:"kind"`
-	MsgId        string `json:"msgId"`
-	Name         string `json:"name"`
-	Patch        []byte `json:"patch"`
+	Checksum string `json:"checksum"`
+	Cluster  string `json:"cluster"`
+	Account  string `json:"account"`
+	Depth    int    `json:"depth"`
+	Kind     string `json:"kind"`
+	MsgId    string `json:"msgId"`
+	Name     string `json:"name"`
+	Patch    []byte `json:"patch"`
 }
 
 type PutObjectMessage struct {
-	ClusterName  string `json:"clusterName"`
-	CustomerGUID string `json:"customerGUID,omitempty" bson:"customerGUID,omitempty"`
-	Depth        int    `json:"depth"`
-	Kind         string `json:"kind"`
-	MsgId        string `json:"msgId"`
-	Name         string `json:"name"`
-	Object       []byte `json:"patch"`
+	Cluster string `json:"cluster"`
+	Account string `json:"account"`
+	Depth   int    `json:"depth"`
+	Kind    string `json:"kind"`
+	MsgId   string `json:"msgId"`
+	Name    string `json:"name"`
+	Object  []byte `json:"patch"`
 }
 
 type VerifyObjectMessage struct {
-	Checksum     string `json:"checksum"`
-	ClusterName  string `json:"clusterName"`
-	CustomerGUID string `json:"customerGUID,omitempty" bson:"customerGUID,omitempty"`
-	Depth        int    `json:"depth"`
-	Kind         string `json:"kind"`
-	MsgId        string `json:"msgId"`
-	Name         string `json:"name"`
+	Checksum string `json:"checksum"`
+	Cluster  string `json:"cluster"`
+	Account  string `json:"account"`
+	Depth    int    `json:"depth"`
+	Kind     string `json:"kind"`
+	MsgId    string `json:"msgId"`
+	Name     string `json:"name"`
 }
 
 type ServerConnectedMessage struct {
-	ClusterName  string `json:"clusterName"`
-	CustomerGUID string `json:"customerGUID,omitempty" bson:"customerGUID,omitempty"`
-	Depth        int    `json:"depth"`
-	MsgId        string `json:"msgId"`
+	Cluster string `json:"cluster"`
+	Account string `json:"account"`
+	Depth   int    `json:"depth"`
+	MsgId   string `json:"msgId"`
 }
