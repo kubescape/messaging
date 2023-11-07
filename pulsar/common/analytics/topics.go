9f6d0fa8c87c56f001ffa462317b4ef6fea617b5
Alessio Greggi <ale_grey_91@hotmail.it>
2023-11-07 15:58:46 +0100

    feat: add analytics ingester consts
    Signed-off-by: Alessio Greggi <ale_grey_91@hotmail.it>

---
+++ pulsar/common/analytics/topics.go
@@ -0,0 +1,13 @@
+package analytics
+
+const (
+	// topic names
+	AnalyticsTopic = "analytics"
+)
+
+// other consts
+const (
+	platformProperty    = "platform"
+	environmentProperty = "environment"
+	googlePlatform      = "google"
+)
