8349e64bdb87c72dc6617560df5c591378ff997f
kooomix <eranm@armosec.io>
2024-03-11 12:15:06 +0200

    Delete attack chains and constants files

---
+++ pulsar/common/properties.go
@@ -0,0 +1,16 @@
+package common
+
+// --------- Ingesters structs and consts -------------
+
+// supported topics and properties:
+// [topic]/[propName]/[propValue]
+
+// attack-chain-scan-state-v1/action/update
+// attack-chain-viewed-v1/action/update
+
+// pulsar messaging properties and values
+const (
+	MsgPropAction            = "action"
+	MsgPropActionValueUpdate = "update"
+	NsgPropActionValueDelete = "delete"
+)
