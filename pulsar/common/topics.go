a73d4993f71d52896dfbcd0aa477ff54dacef462
kooomix <eranm@armosec.io>
2024-03-11 12:15:15 +0200

    Remove unused code related to ingesters and messaging properties

--- pulsar/common/topics.go
+++ pulsar/common/topics.go
@@ -12,18 +12,3 @@ const (
 	AttackChainScanReportFinishTopic = "attack-chain-scan-report-finished-v1"
 	SynchronizerFinishTopic          = "synchronizer-finished-v1"
 )
-
-// --------- Ingesters structs and consts -------------
-
-// supported topics and properties:
-// [topic]/[propName]/[propValue]
-
-// attack-chain-scan-state-v1/action/update
-// attack-chain-viewed-v1/action/update
-
-// pulsar messaging properties and values
-const (
-	MsgPropAction            = "action"
-	MsgPropActionValueUpdate = "update"
-	NsgPropActionValueDelete = "delete"
-)
